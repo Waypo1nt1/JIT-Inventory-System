eebdffbb51aef8e0e8e05d435695386cd4e48f3e
Popova-Ekaterina <popovsasa997@gmail.com>
2026-05-21 07:03:12 +0300

    build package added

--- src/index.js
+++ src/index.js
@@ -11,16 +11,16 @@ suppliesService.init(DATA_FILE_PATH);
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.use('/supplies', suppliesRouter);
 
-app.use((req, res) => {
-    res.status(404).json({ error: 'Маршрут не найден' });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: 'Внутренняя ошибка сервера' });
-});
+app.use((req, res) => { res.status(404).json({ error: 'Маршрут не найден' }); });
+app.use((err, req, res, next) => { res.status(500).json({ error: 'Ошибка сервера' }); });
 
 app.listen(PORT, () => {
     console.log(`API складов JIT запущено: http://localhost:${PORT}`);
